2791a7cecc383c0ac969a0a2d208d2673c2ef639
Suliman <sulimanrageb015@gmail.com>
2026-09-02 01:16:18 +0300

    Task 01 : Smart Product Search

---
+++ Assignment Advanced 02/Product.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_Advanced_02
+{
+    internal class Product
+    {
+            //public Product(int id, string name, string category, double price, int stock)
+            //{
+            //    Id = id;
+            //    Name = name;
+            //    Category = category;
+            //    Price = price;
+            //    Stock = stock;
+            //}
+
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Category { get; set; }
+            public double Price { get; set; }
+            public int Stock { get; set; }
+
+            public static List<Product> SearchProducts(List<Product> product, Func<Product, bool> valid)
+            {
+                List<Product> searchproducts = new List<Product>();
+                foreach (Product p in product)
+                {
+                    if (valid(p))
+                        searchproducts.Add(p);
+
+                }
+                return searchproducts;
+            }
+
+
+    }
+
+}
--- Assignment Advanced 02/Program.cs
+++ Assignment Advanced 02/Program.cs
@@ -1,1 +1,44 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+using Assignment_Advanced_02;
+
+List<Product> catalog = new()
+{
+    new Product { Id=1, Name="Laptop", Category="Electronics", Price=1200, Stock=10 },
+    new Product { Id=2, Name="Phone", Category="Electronics", Price=800, Stock=25 },
+    new Product { Id=3, Name="T-Shirt", Category="Clothing", Price=30, Stock=100 },
+    new Product { Id=4, Name="Jeans", Category="Clothing", Price=60, Stock=50 },
+    new Product { Id=5, Name="Chocolate", Category="Food", Price=5, Stock=200 },
+    new Product { Id=6, Name="Coffee Beans", Category="Food", Price=15, Stock=80 },
+    new Product { Id=7, Name="C# Book", Category="Books", Price=45, Stock=30 },
+    new Product { Id=8, Name="Novel", Category="Books", Price=20, Stock=60 },
+    new Product { Id=9, Name="Headphones", Category="Electronics", Price=150, Stock=40 },
+    new Product { Id=10, Name="Jacket", Category="Clothing", Price=120, Stock=15 }
+};
+Func<Product, bool> searchvalid1 = (Elec) => Elec.Category.Equals("Electronics");
+Func<Product, bool> searchvalid2 = (price) => price.Price < 50;
+Func<Product, bool> searchvalid3 = (stock) => stock.Stock > 0;
+Func<Product, bool> searchvalid4 = (p) => p.Category == "Clothing" && p.Price < 100;
+List<Product> result1 = Product.SearchProducts(catalog, searchvalid1);
+Console.WriteLine("- - -Electronics- - -");
+foreach (Product product in result1)
+{
+    Console.WriteLine($"{product.Name}-${product.Price} (Stock:{product.Stock})");
+}
+List<Product> result2 = Product.SearchProducts(catalog, searchvalid2);
+Console.WriteLine("\n- - -Under $50- - -");
+foreach (Product product in result2)
+{
+    Console.WriteLine($"{product.Name}-${product.Price} (Stock:{product.Stock})");
+}
+List<Product> result3 = Product.SearchProducts(catalog, searchvalid3);
+Console.WriteLine("\n- - -In Stock- - -");
+foreach (Product product in result3)
+{
+    Console.WriteLine($"{product.Name}-${product.Price} (Stock:{product.Stock})");
+}
+List<Product> result4 = Product.SearchProducts(catalog, searchvalid4);
+Console.WriteLine("\n- - -Clothing Under 100$- - -");
+foreach (Product product in result4)
+{
+    Console.WriteLine($"{product.Name}-${product.Price} (Stock:{product.Stock})");
+}
